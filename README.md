5a527d64c3f8bf28cb44af3ec10dcb20a2ca9d30
Ruchi Jain <ruchijn@outlook.com>
2017-03-08 14:21:29 -0500

    Updated README to have commands to run for various scenarios, updated package json with required package, created a src folder for all the source files.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # react-node-chat
 Chat app with react front end and node/socket.io backend
+
+********FE details start********
+
+Folder structure:
+* All source files go inside src
+* The output folder "dist" will sit parallel to src
+
+
+To run the code:
+* To build the app run -> npm run build
+* To clean the dist folder run -> npm run clean
+* To clean, build app and run it in watch mode -> npm run dev
+
+********FE details end********
---
+++ client/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {reducer as reduxFormReducer} from 'redux-form';
+import reduxThunk from 'redux-thunk';
+
+import allReducers from './reducers';
+
+import { Route, Router, IndexRoute, browserHistory } from 'react-router';
+import { createHashHistory } from 'history';
+import Routes from './router';
+
+// Variable store for loading all reducers
+const store = createStore(allReducers);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
+ReactDOM.render(
+  <Localization messages={localizationBundle}>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </Localization>,
+  document.getElementById('app')
+);
